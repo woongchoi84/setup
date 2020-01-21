8009feb3d657eb5becccf0165d29da85af778555
woongchoi84 <woongchoi84@gmail.com>
2020-01-21 18:28:36 +0900

    w/o comment

---
+++ pythonPackages/newspaper.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# ==================================================
+#  firefox downgrade
+# ==================================================
+# [Make Backup File]
+sudo pip3 install gensim newspaper3k
+
+# [Completed Message]
+echo "=================================================="
+echo " News Paper Summarizer has been installed"
+echo "=================================================="
